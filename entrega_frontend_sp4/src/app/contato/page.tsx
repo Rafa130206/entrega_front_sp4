47d5d26b8a5d174bdcbd1bbf818e0df5b4437722
Rafa130206 <rafabompadre@gmail.com>
2024-11-04 14:00:51 -0300

    Página de contato concluída

---
+++ entrega_frontend_sp4/src/app/contato/page.tsx
@@ -0,0 +1,26 @@
+"use client"; 
+
+export default function Contato() {
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className='text-center pt-2 text-[3vw] md:text-4xl lg:text-5xl font-bold'>Entre em contato conosco!</h1>
+            <section className='m-5 w-full flex justify-center'>
+                <form action="contato" method='get' className="flex flex-col w-[25vw] md:w-[300px] lg:w-[440px] bg-gray-200 rounded-lg p-5">
+                    <fieldset className="border-none flex flex-col">
+                        <legend className="p-4 text-2xl font-bold text-center">Dados Pessoais</legend>
+                        <label className="pt-2 pb-2 block">
+                            <input type="text" name="nome" id="idnome" placeholder='Insira seu nome' className="text-lg w-full p-1 rounded-md border-none" />
+                        </label>
+                        <label className="pt-2 pb-2 block">
+                            <input type="text" name='email' id='idemail' placeholder='Insira seu email' className="text-lg w-full p-1 rounded-md border-none" />
+                        </label>
+                        <label className="pt-2 pb-2 block">
+                            <textarea name="mensagem" id="idmensagem" cols={30} rows={10} placeholder='Deixe sua mensagem!' className="text-lg w-full p-1 rounded-md border-none"></textarea>
+                        </label>
+                    </fieldset>
+                    <button type='submit' value={"Enviar"} className="mt-4 mb-6 text-xl p-2 rounded-lg bg-gray-400 hover:bg-gray-600 text-white cursor-pointer">Enviar</button>
+                </form>
+            </section>
+        </div>
+    );
+}
