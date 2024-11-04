c48fda9e5e6c2df4b0ae4bce5b88b1457811c209
Rafa130206 <rafabompadre@gmail.com>
2024-11-04 13:50:26 -0300

    Responsividade página de integrantes concluída

--- entrega_frontend_sp4/src/app/pgInt/page.tsx
+++ entrega_frontend_sp4/src/app/pgInt/page.tsx
@@ -6,17 +6,17 @@ import Rafael from '../img/Rafael.jpg';
 export default function Integrantes() {
     return (
         <div className="flex flex-col items-center">
-            <h1 className="text-4xl font-bold underline text-center m-8">Página de Integrantes</h1>
-            <h2 className="text-center mt-6 mb-6 text-2xl">REPOSITÓRIO</h2>
+            <h1 className="text-3xl sm:text-4xl font-bold underline text-center m-8 mb-6">Página de Integrantes</h1>
+            <h2 className="text-center my-2 sm:my-6 text-xl sm:text-2xl">REPOSITÓRIO</h2>
             <div className="text-center mb-6">
                 <p>
-                    <a href="https://github.com/Rafa130206/entrega_front_sp4" target="_blank" rel="noopener noreferrer" className="text-blue-500 no-underline">https://github.com/Rafa130206/entrega_front_sp4</a>
+                    <a href="https://github.com/Rafa130206/entrega_front_sp4" target="_blank" rel="noopener noreferrer" className="text-blue-500 no-underline sm:text-xl lg:2xl">https://github.com/Rafa130206/entrega_front_sp4</a>
                 </p>
             </div>
-            <div className="flex flex-row justify-evenly mt-6 mb-20 gap-60">
+            <div className="flex flex-col lg:flex-row justify-evenly mt-6 mb-20 gap-16 lg:gap-24 xl:gap-52">
                 <section className="text-center flex flex-col items-center">
-                    <Image src={Lucas} alt="Lucas" className="w-[15vw] shadow-lg" />
-                    <p className="mt-2 underline text-lg">
+                    <Image src={Lucas} alt="Lucas" className="w-7/12 sm:w-5/12 lg:w-[15vw] shadow-lg" />
+                    <p className="mt-2 underline text-lg sm:text-2xl lg:text-lg">
                         Nome: Lucas Lerri de Almeida<br />
                         RM: 554635<br />
                         Turma: 1TDSPI<br />
@@ -24,18 +24,16 @@ export default function Integrantes() {
                     </p>
                 </section>
                 <section className="text-center flex flex-col items-center">
-                    <Image src={Joao} alt="Joao" className="w-[15vw] shadow-lg" />
-                    <p className="mt-2 underline text-lg">
-                        Nome: João Pedro C. Zanni<br />
+                    <Image src={Joao} alt="Joao" className="w-7/12 sm:w-5/12 lg:w-[15vw] shadow-lg" />
+                    <p className="mt-2 underline text-lg sm:text-2xl lg:text-lg"> João Pedro C. Zanni<br />
                         RM: 557591<br />
                         Turma: 1TDSPI<br />
                         <a href="https://github.com/JoaoPedroCZanni" className="text-blue-500 no-underline">https://github.com/JoaoPedroCZanni</a>
                     </p>
                 </section>
                 <section className="text-center flex flex-col items-center">
-                    <Image src={Rafael} alt="Rafael" className="w-[15vw] shadow-lg" />
-                    <p className="mt-2 underline text-lg">
-                        Nome: Rafael Bompadre Lima<br />
+                    <Image src={Rafael} alt="Rafael" className="w-7/12 sm:w-5/12 lg:w-[15vw] shadow-lg" />
+                    <p className="mt-2 underline text-lg sm:text-2xl lg:text-lg"> Rafael Bompadre Lima<br />
                         RM: 556459<br />
                         Turma: 1TDSPH<br />
                         <a href="https://github.com/Rafa130206" className="text-blue-500 no-underline">https://github.com/Rafa130206</a>
