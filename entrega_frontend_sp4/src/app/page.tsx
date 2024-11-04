28f96f7c998de7ccdf507ce5df7045eab49247f5
Rafa130206 <rafabompadre@gmail.com>
2024-11-04 11:38:10 -0300

    Adição de novas imagens

--- entrega_frontend_sp4/src/app/page.tsx
+++ entrega_frontend_sp4/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">JLR Soluções</h1>
+      <h1 className="text-3xl font-bold underline text-center m-8">JLR Soluções</h1>
     </div>
   );
 }
